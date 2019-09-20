2a6f6c343fc61f473195dd70f620b49156052770
ginsm <mattg.0609@gmail.com>
2019-09-19 20:46:59 -0400

    Adding a commander-based test app

---
+++ tests/test.js
@@ -0,0 +1,12 @@
+// Initialize commander
+const cmdr = require('commander');
+const validate = require('../lib/validate');
+
+cmdr
+    .option('hasRule', '')
+    .option('noRule', '')
+    .parse(process.argv)
+
+validate
+    .rule('hasRule', ['string'], ['number', 'string'], 2, true)
+    .parseCmdr(cmdr);
