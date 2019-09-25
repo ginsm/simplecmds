73658c29694e3b5a529ccf68991238539778005c
Syh <Syh@OSX.local>
2019-09-25 03:02:23 -0400

    Updating rule to apply string notation for arg types

--- tests/test.js
+++ tests/test.js
@@ -2,11 +2,11 @@ const cmdr = require('commander');
 const validate = require('../lib/validate');
 
 cmdr
-    .option('hasRule', '')
-    .option('noRule', '')
+    .option('hasRule')
+    .option('noRule')
     .parse(process.argv);
 
-
 validate
-    .rule('hasRule', ['string'], ['number', 'string'], 2)
-    .parseCmdr(cmdr);
+    .rule('hasRule', '<number> <string,number> [number]', 2)
+    .parse(cmdr);
+
