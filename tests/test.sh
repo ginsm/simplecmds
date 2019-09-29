c02884cb6379e039d23f2e26e6ce8475cbfd5a88
ginsm <matt@mgin.me>
2019-09-29 07:29:44 -0400

    Updating formatting; adding 2 more tests (Bool cmd, Multiple Rules)

--- tests/test.sh
+++ tests/test.sh
@@ -6,33 +6,54 @@ RED="\033[0;31m"
 NC="\033[0m"
 
 # ---------------------------------- Arrays ----------------------------------- #
-# Arguments Arrays
-args=("1 two" "1 two 3 4" "1 two 3" "1" "1 two 3 4 5" "two 1 three four five" "1 two 3 4")
+# Command
+cmd=("node example")
 
-# Commands Arrays
-cmd=("node example hasRule")
+# Arguments
+args=("hasRule 1 two" 
+      "hasRule 1 two 3 4" 
+      "hasRule 1 two 3" 
+      "hasRule 1 two 3 4" 
+      "bool"
+      "hasRule 1" 
+      "hasRule 1 two 3 4 5" 
+      "hasRule two 1 three four five"
+      "")
 
-notations=("hasRule <num> <num/str> [num]")
+# Test Names
+testNames=("Required             " 
+           "Optional (2)         " 
+           "Optional (1)         " 
+           "Multiple Rules       " 
+           "Bool Command         " 
+           "Missing required     " 
+           "Too many             " 
+           "Wrong type           "
+           "No cmd or args       ") 
 
-# Test Names & Expected Arrays
-testNames=("Required             " "Optional (2)         " "Optional (1)         " "Missing required     " "Too many             " "Wrong type           " "Multiple Rules       ") 
-expected=("      true" "      true" "      true" "     false" "     false" "     false" "      true")
+# Expected result
+expected=("      true" 
+          "      true" 
+          "      true" 
+          "      true" 
+          "      true" 
+          "     false" 
+          "     false" 
+          "     false"
+          "     ")
 
 
 # ------------------------------- Begin Printing ------------------------------ #
 printf "\n${BLUE}»         Argument Validation         «${NC}\n\n"
-printf "${LBLUE}Arguments            Result    Expected${NC}\n\n"
-printf "${LBLUE}» ${notations[0]}${NC}\n"
+printf "${LBLUE}Arguments            Result    Expected${NC}\n"
 
-for ((i = 0 ; i < 7 ; i++)); do
+for ((i = 0 ; i < 9 ; i++)); do
 	command="$(${cmd[0]} ${args[i]})"
-  echo "Args: ${args[i]}"
 	if [[ "${command}" == "${expected[i]//[^*a-z]/}" ]]; then
 	 	printf "${GREEN}${testNames[i]}${command}${expected[i]}${NC}\n"
 	else
 	 	printf "${RED}${testNames[i]}${command}${expected[i]}${NC}\n"
 	fi
-  echo " "
 done
 
 echo ""
