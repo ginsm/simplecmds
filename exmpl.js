361cc001444fb874a613f2638ac5f0d4f29d1d73
ginsm <matt@mgin.me>
2019-10-16 04:58:26 -0400

    Shifting --no-rule flag to bottom.

--- exmpl.js
+++ exmpl.js
@@ -2,11 +2,11 @@ const cmds = require('./cmds');
 
 console.time('Build time');
 cmds
-    .command('-c --create <text>', 'create a task')
+    .command('-c --create <text>', 'Create a task')
     .rule('<number,string>', 1)
-    .command('-n --no-rule', 'No rule contained')
-    .command('-d --delete <id> [id]', 'delete a task')
+    .command('-d --delete <id> [id]', 'Delete a task')
     .rule('<number> [number]', 1)
+    .command('-n --no-rule', 'No rule contained (testing)')
     .parse(process.argv);
 console.timeEnd('Build time');
 
