b78119a2f20021f2d01ecc67dc1665a2b1bbfb4b
ginsm <matt@mgin.me>
2019-10-18 04:33:13 -0400

    Upping delete rule's arg amount

--- exmpl.js
+++ exmpl.js
@@ -5,7 +5,7 @@ cmds
     .command('-c --create <text>', 'Create a task')
     .rule('<number,string>', 1)
     .command('-d --delete <id> [id]', 'Delete a task')
-    .rule('<number> [number]', 1)
+    .rule('<number> [number]', 2)
     .command('-n --no-rule', 'No rule contained (testing)')
     .parse(process.argv);
 console.timeEnd('Build time');
