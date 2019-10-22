02e3964151999000ccbdb22b9f56c2a767e592c5
ginsm <matt@mgin.me>
2019-10-22 07:23:51 -0400

    Adding a callback function

--- exmpl.js
+++ exmpl.js
@@ -1,7 +1,7 @@
 const cmds = require('./cmds');
 
 cmds
-    .command('-c --create <text>', 'Create a task')
+    .command('-c --create <text>', 'Create a task', console.log)
     .rule('<number,string>', 1)
     .command('-d --delete <id> [id]', 'Delete a task')
     .rule('<number> <number> [number]', 3)
