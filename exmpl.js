02773c3baf16ee6e790395ee019744a022479f62
ginsm <matt@mgin.me>
2019-10-23 13:18:59 -0400

    Adding program description example

--- exmpl.js
+++ exmpl.js
@@ -2,6 +2,7 @@ const cmds = require('./cmds');
 
 cmds
     .setVersion('v0.8.9')
+    .description('An example project using cmds.js.')
     .command('-c --create <text>', 'Create a task')
     .rule('<number,string>', 1)
     .command('-d --delete <id> [id]', 'Delete a task')
