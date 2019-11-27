21b0ed4745db27ed832effabc6dd3608ed3016e1
ginsm <matt@mgin.me>
2019-11-27 01:13:24 -0500

    Add non-verbose and verbose test scripts

--- tests/test-script.sh
+++ tests/test-script.sh
@@ -6,65 +6,79 @@ cmd="node $wdir/test-interface"
 
 
 # SECTION - Importing
-source $wdir/utility.sh
+source $wdir/utility.sh $1
+success="${PASS}Passed${RESTORE}"
 
 
 # SECTION - Script Header
 header "Simplecmds Test Script"
 printf "These tests are designed to ensure that the packages functionality is working
-as intended. It uses an interface that was created for the tests to cover all of the
-functionality (test-interface.js)."
+as intended. It uses an interface that was created via simplecmds specifically
+for the tests."
 newline 2
 
 
 # SECTION - Test Definitions
 header "Type Checking"
 
-define "Command expecting string returns true when given string" \
-    "true" \
-    "$cmd -t string"
+  define "Command expecting string returns true when given string" \
+      "true" \
+      "$cmd -t string"
 
-define "Command expecting string returns false when given number" \
-    "false" \
-    "$cmd -t 42"
+  define "Command expecting string returns false when given number" \
+      "false" \
+      "$cmd -t 42"
 
-define "Command expecting bool returns true when no arguments are given" \
-    "true" \
-    "$cmd -b"
+  define "Command expecting bool returns true when no arguments are given" \
+      "true" \
+      "$cmd -b"
 
-define "Command expecting bool returns false when given arguments" \
-    "false" \
-    "$cmd -b 0"
+  define "Command expecting bool returns false when given arguments" \
+      "false" \
+      "$cmd -b 0"
 
-define "Concatenated arguments work and validate properly." \
-"false
+  define "Concatenated arguments work and validate properly." \
+  "false
 true" \
-    "$cmd -tn 30,20"
+      "$cmd -tn 30,20"
 
-define "Command accepting number or string returns true when given number" \
-    "true" \
-    "$cmd -A 20"
+  define "Command accepting number or string returns true when given number" \
+      "true" \
+      "$cmd -A 20"
 
-define "Command accepting number or string returns false when given no arguments" \
-    "false" \
-    "$cmd -A"
+  define "Command accepting number or string returns false when given no arguments" \
+      "false" \
+      "$cmd -A"
 
-define "Command expecting multiple arguments/types returns true when given proper arguments" \
-    "true" \
-    "$cmd -m 'string' 20"
+  define "Command expecting multiple arguments/types returns true when given proper arguments" \
+      "true" \
+      "$cmd -m 'string' 20"
 
-define "Command expecting multiple arguments/types returns false when given improper arguments" \
-    "false" \
-    "$cmd -m 20 'string'"
-
+  define "Command expecting multiple arguments/types returns false when given improper arguments" \
+      "false" \
+      "$cmd -m 20 'string'"
 
 
 
 header "Argument Amount"
 
-define "Command enforcing at most 3 arguments returns 3 for amount of arguments when given 4 arguments" \
-    "3" \
-    "$cmd -a -l one two three four"
+  define "Command enforcing at most 3 arguments returns 3 for amount of arguments when given 4 arguments" \
+      "3" \
+      "$cmd -a -l one two three four"
 
 
+end=`date +%s%N`
+
+header "Summary"
+printf "Status: ${success}"
 newline
+printf "Passed: ${HEADER}${passed}${RESTORE}"
+newline
+printf "Failed: ${HEADER}${failed}${RESTORE}"
+
+newline 2
+
+if [[ "${verbose}" == "false" ]]; then
+  printf "Run '${HEADER}npm run test:verbose${RESTORE}' to see all of the test definitions."
+  newline 2
+fi
--- tests/utility.sh
+++ tests/utility.sh
@@ -5,29 +5,48 @@ PASS='\033[01;32m'
 FAIL='\033[01;31m'
 HEADER='\033[01;33m'
 
+# SECTION - Variables
+count=0
+passed=0
+failed=0
+verbose=$1
+
 
 # SECTION - Test definition tool
 function define() {
   local desc="${1}"
   local expected="${2}"
   local result=$(${3})
-  
-  printf "${DESC}${desc}${RESTORE}"
-  newline
 
   if [[ "${result}" == "${expected}" ]]; then
-    printf "Status: ${PASS}Passed${RESTORE}"
+    if [[ "${verbose}" == "true" ]]; then
+      printf "${DESC}${desc}${RESTORE}"
+      newline
+
+      printf "Status: ${PASS}Passed${RESTORE}"
+      newline 2
+    fi
+    passed=$(($passed + 1))
+
   else
+    printf "${DESC}${desc}${RESTORE}"
+    newline
+
     printf "Status: ${FAIL}Failed${RESTORE}"
+    newline 2
+
+    success="${FAIL}Failed${RESTORE}"
+    failed=$(($failed + 1))
+    
   fi
 
-  newline 2
+  count=$(($count + 1))
 }
 
 
 # SECTION - Helper Functions
 function newline() {
-  if [[ ! $1 ]]; then
+  if [[ ! ${1} ]]; then
     printf "\n"
   elif (( $1 > 1 )); then 
     for i in $(seq $1); do
