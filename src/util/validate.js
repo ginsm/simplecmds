8c8c0a612cb4d40abe1a30b1f2afe027b1f21b14
ginsm <matt@mgin.me>
2019-12-03 07:36:56 -0500

    Use new .rules property instead of .rule

--- src/util/validate.js
+++ src/util/validate.js
@@ -1,21 +1,21 @@
 /**
  * Validate the types and amount of args for a command.
- * @param {[]} obj - Command arguments and rule.
+ * @param {[]} obj - Command arguments and rules.
  * @param {string} cmdName - Name of the command.
  * @return {boolean} Validity represented by a boolean.
  */
-function typeCheck({args, rule}, cmdName) {
-  const required = rule.filter((rule) => rule[0] === '<');
+function typeCheck({args, rules}, cmdName) {
+  const required = rules.filter((rule) => rule[0] === '<');
   const validRequiredAmount = args.length >= required.length;
-  const lastNotation = rule.slice(-1)[0];
+  const lastNotation = rules.slice(-1)[0];
 
   // Check types
-  const valid = (rule, arg) => rule.includes(typeof arg);
+  const valid = (rules, arg) => rules.includes(typeof arg);
   const validTypes = args.map((arg, id) => {
     const idRequired = (required.length - 1 >= id);
-    const noNotation = (id > rule.length - 1);
-    return idRequired ? valid(rule[id], arg) : !idRequired &&
-              (noNotation ? valid(lastNotation, arg) : valid(rule[id], arg));
+    const noNotation = (id > rules.length - 1);
+    return idRequired ? valid(rules[id], arg) : !idRequired &&
+          (noNotation ? valid(lastNotation, arg) : valid(rules[id], arg));
   }).every((arg) => arg === true);
 
   return validRequiredAmount && validTypes;
