3a8d2eb567a49d4844f1bd02ff8255533cc4e8c5
ginsm <matt@mgin.me>
2019-12-05 05:41:13 -0500

    Finish module
    The module houses two types of help pages; the main help
    page and single command help pages.

--- src/util/helpmenu.js
+++ src/util/helpmenu.js
@@ -1,54 +1,66 @@
-const Help = {
+// SECTION - Imports
+const basename = require('path').basename;
+const {longest, capitalize} = require('./helper');
+
+
+const HelpMenu = {
+  // SECTION - Help Pages
+
   /**
-   * Generate a single commands help menu string.
-   * @param {{}} builderObj - The commands builder object.
-   * @param {string} longestUsage - The longest command usage string's length.
-   * @return {string} - The help item.
+   * Outputs the main help page (all commands).
+   * @param {[{}]} options - Every commands' build options.
    */
-  getCommandHelp({usage, description = ''}, longestUsage) {
-    const spaces = Array((longestUsage + 4) - usage.length).join(' ');
-    return `${usage} ${spaces} ${description}`;
+  mainPage(options) {
+    const {helpString, programName} = HelpMenu;
+    const length = longest(options.map((obj) => obj.usage)).length;
+    const helpStrings = options.map(helpString(length));
+
+    [`\nProgram: ${capitalize(programName)} (${this.version})`,
+      this.description && `Description: ${this.description}\n` || '',
+      'Commands:',
+      ...helpStrings.slice(0, this.debug ? -3 : -2),
+      `\nDefaults:`,
+      ...helpStrings.slice(this.debug ? -3 : -2),
+      `\nUsage: ${programName} <command> [...args]\n`,
+    ].forEach((line) => console.log(line));
+  },
+
+
+  /**
+   * Outputs a single command's help page.
+   * @param {*} obj - The command's build object.
+   */
+  singleCommandPage(obj) {
+    const {programName} = HelpMenu;
+    [`\nProgram: ${capitalize(programName)} (${this.version})`,
+      `Command: ${obj.alias.join(', ')}\n`,
+      `${obj.helpPage || 'There is no help page for this command.'}\n`,
+      `Usage: ${programName} ${obj.usage}\n`,
+    ].forEach((line) => console.log(line));
+  },
+
+
+  // SECTION - Helpers
+
+  /**
+   * Generate a command's help string.
+   * @param {string} length - The longest usage string's length.
+   * @return {string} A generated command help string.
+   */
+  helpString(length) {
+    return ({usage, description = ''}) => {
+      const spaces = Array((length + 4) - usage.length).join(' ');
+      return `${usage} ${spaces} ${description}`;
+    };
+  },
+
+
+  /**
+   * @return {string} The name of the program.
+   */
+  get programName() {
+    return basename(process.argv[1], '.js');
   },
 };
 
-module.exports = Help;
-
-
-/* TODO - Goal Help Menu
-
-
-————————————————————————————————————————
-            Main Help Menu
-————————————————————————————————————————
-
-Program: <Program> (version)
-Description: <description>
-
-Commands:
-...commands
-
-Defaults:
-...defaults
-
-Usage: <program> <command> [args]
-
-————————————————————————————————————————
-
-
-————————————————————————————————————————
-            Single Command
-————————————————————————————————————————
-
-Program: <Program> (version)
-Command: <command>
-Aliases: <...aliases>
-
-Command Help:
-...command.help
-
-Usage: <program> <command.usage>
-
-————————————————————————————————————————
-
-
-*/
+module.exports = HelpMenu;
