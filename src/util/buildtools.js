96e2ff5847d9f2dde421729087bd248871eab4bd
ginsm <matt@mgin.me>
2019-12-05 05:37:32 -0500

    Add default commands in this module instead of build

--- src/util/buildtools.js
+++ src/util/buildtools.js
@@ -5,22 +5,64 @@ const BuildTools = {
   /**
    * Initiates the build tools and ensures there are no user errors.
    * @param {[]} entries - Entries for each command.
+   * @param {{}} that - `this` context of simplecmds.js.
    */
-  init(entries = []) {
+  init(entries = [], that) {
     BuildTools.noConflictingAliases(entries);
     BuildTools.validateRules(entries);
+    const defaults = this.addDefaultCommands(that);
     Object.assign(BuildTools, {
-      commands: Object.fromEntries(entries),
+      commands: {
+        ...Object.fromEntries(entries),
+        ...defaults,
+      },
     });
   },
 
+  // SECTION - Default Commands
+  /**
+   * Add the default commands to the Builder object.
+   * @param {{}} that - `this` context of simplecmds.js.
+   * @return {{}} An object containing the default commands.
+   */
+  addDefaultCommands(that) {
+    const alias = this.defaultShortAlias();
+    return {
+      help: {
+        description: 'Output help menu.',
+        alias: [alias.help, '--help'],
+        usage: `${alias.help} --help`,
+        callback: function([command]) {
+          that.showHelp.call(that, {exit: true, command});
+        },
+        rules: '<number,string>',
+        amount: 1,
+      },
+      vers: {
+        description: 'Output version information.',
+        alias: [alias.version, '--version'],
+        usage: `${alias.version} --version`,
+        callback: () => console.log(that.version),
+      },
+      ...(that.debug && {debug: {
+        description: 'Output debug information.',
+        alias: [alias.debug, '--debug'],
+        usage: `${alias.debug} --debug`,
+        callback: () => console.log(this.commands),
+      }}),
+    };
+  },
 
+
+  // SECTION - Error checking
   /**
    * Ensure there are no conflicting aliases.
    * @param {[]} entries - Entries for each command.
    */
   noConflictingAliases(entries = []) {
     const aliases = BuildTools.allAliases(entries);
+    Object.assign(BuildTools, {aliases});
+
     // ensure there are no duplicate aliases
     (new Set(aliases)).size === aliases.length || error('Creation',
         'You have conflicting (duplicate) aliases in your commands.',
@@ -50,6 +92,22 @@ const BuildTools = {
     });
   },
 
+
+  // SECTION - Helpers
+  /**
+   * Capitalizes default if there's a conflicting alias.
+   * @return {{}} Non-conflicting aliases for default commands.
+   */
+  defaultShortAlias() {
+    const resolve = (alias) => {
+      return this.aliases.includes(alias) ? alias.toUpperCase() : alias;
+    };
+    return ({
+      help: resolve('-h'),
+      version: resolve('-v'),
+      debug: resolve('-d'),
+    });
+  },
 
   /**
    * Get an array of all the aliases.
