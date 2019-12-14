5342fa2283557916f4ff90b501abada493880c51
ginsm <matt@mgin.me>
2019-12-13 23:08:17 -0500

    Fix JSDoc description; add @memberof <module>
    This property will help find which module an imported function originates from.

--- src/util/parse.js
+++ src/util/parse.js
@@ -9,6 +9,7 @@ const Parsing = {
    * @return {{}} Generated object containing args.
    * @example
    * { myCommand: [1, 2] }
+   * @memberof parse.js
    */
   parseArgs(args, commands) {
     return args
@@ -38,12 +39,13 @@ const Parsing = {
   },
 
   /**
-   * Expands concatArg aliases and arguments.
+   * Expands concatenated aliases and arguments.
    * @param {[*]} arr - Process.argv
    * @return {[*]} Aliases and arguments expanded in place.
    * @example
    * expandAliases(['-l', 'one', '-abc', '1,2,3']);
    * // output -> ['-l', 'one', '-a', 1, '-b', 2, '-c', 3]
+   * @memberof parse.js
    */
   expandAliases(arr) {
     return flatten(arr.map((arg, id, arr) => {
@@ -51,7 +53,7 @@ const Parsing = {
       if (groupedAliases) {
         const aliases = arg.replace(/(?<!\W)(?=\w)/g, '-').split(/(?=\W)/g);
         const groupedArgs = /\w+(,\w+)+/g.test(arr[id + 1]);
-        const args = groupedArgs && arr[id + 1].split(',') || [];
+        const args = groupedArgs ? arr[id + 1].split(',') : [];
         return alternate(aliases, args);
       }
       // set grouped arguments as undefined so it can be removed with filter
@@ -67,6 +69,7 @@ const Parsing = {
    * @example
    * generateAlias('-m --my-command [number]', 'myCommand');
    * // output -> ['-m', '--my-command']
+   * @memberof parse.js
    */
   generateAlias(usage, command) {
     const aliases = /(?<!\S)(-\w\b|--[\w-]{3,}|(?=[^-])[\w-]{2,})/g;
